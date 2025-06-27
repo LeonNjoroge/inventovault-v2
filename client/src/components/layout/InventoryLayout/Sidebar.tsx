366b7fd218d813e6327789624dbf79a191cf0717
LeonNjoroge <leonmuraguri@gmail.com>
2025-06-27 22:49:44 +0300

    Feat: Configure sidebar

--- client/src/components/layout/InventoryLayout/Sidebar.tsx
+++ client/src/components/layout/InventoryLayout/Sidebar.tsx
@@ -14,26 +14,37 @@ const Sidebar = () => {
     const router = useRouter();
 
     return (
-        <aside className="w-64 bg-white h-screen shadow-md fixed left-0 top-0 z-50">
-            <div className="p-6 text-xl font-bold text-blue-600">InventoVault</div>
-            <nav className="mt-4 space-y-2">
-                {navItems.map((item) => (
-                    <Link key={item.href} href={item.href}>
-                        <div
-                            className={`flex items-center px-6 py-3 hover:bg-blue-50 cursor-pointer transition rounded-r-full ${
-                                router.pathname === item.href
-                                    ? "bg-blue-100 text-blue-700 font-semibold"
-                                    : "text-gray-700"
-                            }`}
-                        >
-                            <span className="mr-3">{item.icon}</span>
-                            <span>{item.label}</span>
-                        </div>
-                    </Link>
-                ))}
-            </nav>
+        <aside className="w-64 bg-white h-screen shadow-md fixed left-0 top-0 z-50 flex flex-col justify-between">
+            {/* Brand */}
+            <div>
+                <div className="p-6 text-xl font-bold text-blue-600">InventoVault</div>
+
+                {/* Navigation */}
+                <nav className="mt-4 space-y-2">
+                    {navItems.map((item) => (
+                        <Link key={item.href} href={item.href}>
+                            <div
+                                className={`flex items-center px-6 py-3 mr-2 hover:bg-blue-50 cursor-pointer transition rounded-r-full ${
+                                    router.pathname === item.href
+                                        ? "bg-blue-100 text-blue-700 font-semibold"
+                                        : "text-gray-700"
+                                }`}
+                            >
+                                <span className="mr-3">{item.icon}</span>
+                                <span>{item.label}</span>
+                            </div>
+                        </Link>
+                    ))}
+                </nav>
+            </div>
+
+            {/* Footer */}
+            <footer className=" px-4 py-4 text-center text-sm text-gray-500">
+                &copy; 2025 InventoVault
+            </footer>
         </aside>
     );
+
 };
 
 export default Sidebar;
