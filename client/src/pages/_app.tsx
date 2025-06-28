48890aff259549129c8203cee5f2aa26949d2332
LeonNjoroge <leonmuraguri@gmail.com>
2025-06-28 09:13:26 +0300

    Feat: Setup Apollo Client

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,18 +2,24 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout/InventoryLayout/Layout";
 import HomeLayout from "@/components/layout/HomeLayout/Layout";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/graphql/apolloClient";
+
 
 export default function App({ Component, pageProps, router }: AppProps) {
-    const isLanding = router.pathname === '/';
+    const isLanding = router.pathname === "/";
 
-    return isLanding ? (
-        <HomeLayout>
-            <Component {...pageProps} />
-        </HomeLayout>
-
-    ) : (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+    return (
+        <ApolloProvider client={client}>
+            {isLanding ? (
+                <HomeLayout>
+                    <Component {...pageProps} />
+                </HomeLayout>
+            ) : (
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            )}
+        </ApolloProvider>
     );
 }
